72e9929cf9da2fe8f0a720ea24eaa3fb665cf1db
kipchumba-ket <kipchumbaelly97@gmail.com>
2023-02-15 09:01:35 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,15 @@
 require 'pry'
+require_relative './concerns/memorable'
+require_relative './concerns/findable'
+require_relative './concerns/paramable'
 
 class Song
+
+  include Memorable::InstanceMethods
+  extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
+  extend Findable::ClassMethods
+
   attr_accessor :name
   attr_reader :artist
 
@@ -18,19 +27,19 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
